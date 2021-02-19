9f993c333a3f5761d22fc5a75d06c5294dc70351
Zeyad <zeyadnajjar999@gmail.com>
2021-02-19 21:24:58 +0200

    changed app to

--- src/main/java/com/world/navigator/NavigatorApplication.java
+++ src/main/java/com/world/navigator/NavigatorApplication.java
@@ -3,9 +3,10 @@ package com.world.navigator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class NavigatorApplication {
+public class NavigatorApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(NavigatorApplication.class, args);
   }
